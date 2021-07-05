c7c33480173ad02a667a9d7c5c18ab331a81858f
arpruss <arpruss@gmail.com>
2021-07-05 16:25:11 -0500

    foo

--- images/convert.sh
+++ images/convert.sh
@@ -1,17 +1,22 @@
 CONVERT="inkscape --export-width=200"
 DIR=../app/src/main/res/drawable/
-${CONVERT} --export-png=${DIR}animal_fu.png chick.svg
-${CONVERT} --export-png=${DIR}animal_to.png red_chick.svg
-${CONVERT} --export-png=${DIR}animal_ou.png lion.svg
-${CONVERT} --export-png=${DIR}animal_fu.png chick.svg
-${CONVERT} --export-png=${DIR}animal_hi.png giraffe.svg
-${CONVERT} --export-png=${DIR}animal_ryu.png red_giraffe.svg
-${CONVERT} --export-png=${DIR}animal_kaku.png elephant.svg
-${CONVERT} --export-png=${DIR}animal_uma.png red_elephant.svg
-${CONVERT} --export-png=${DIR}animal_kin.png dog.svg
-${CONVERT} --export-png=${DIR}animal_gin.png cat.svg
-${CONVERT} --export-png=${DIR}animal_nari_gin.png red_cat.svg
-${CONVERT} --export-png=${DIR}animal_kei.png bunny.svg
-${CONVERT} --export-png=${DIR}animal_nari_kei.png red_bunny.svg
-${CONVERT} --export-png=${DIR}animal_kyo.png pig.svg
-${CONVERT} --export-png=${DIR}animal_nari_kyo.png red_pig.svg
+
+animal() {
+    echo $1 to animal_$2
+    ${CONVERT} --export-png=${DIR}animal_$2.png $1.svg
+}
+animal chick fu
+animal red_chick to
+animal lion ou
+animal chick fu
+animal giraffe hi
+animal red_giraffe ryu
+animal elephant kaku
+animal red_elephant uma
+animal dog kin
+animal cat gin
+animal red_cat nari_gin
+animal bunny kei
+animal red_bunny nari_kei
+animal pig kyo
+animal red_pig nari_kyo
