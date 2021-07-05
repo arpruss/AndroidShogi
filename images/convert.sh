264da1e623df3577ea328d283f1d730244a86ea1
arpruss <arpruss@gmail.com>
2021-07-05 16:17:37 -0500

    foo

---
+++ images/convert.sh
@@ -0,0 +1,17 @@
+CONVERT="inkscape --export-width=200"
+DIR=../app/src/main/res/drawable/
+${CONVERT} --export-png=${DIR}animal_fu.png chick.svg
+${CONVERT} --export-png=${DIR}animal_to.png red_chick.svg
+${CONVERT} --export-png=${DIR}animal_ou.png lion.svg
+${CONVERT} --export-png=${DIR}animal_fu.png chick.svg
+${CONVERT} --export-png=${DIR}animal_hi.png giraffe.svg
+${CONVERT} --export-png=${DIR}animal_ryu.png red_giraffe.svg
+${CONVERT} --export-png=${DIR}animal_kaku.png elephant.svg
+${CONVERT} --export-png=${DIR}animal_uma.png red_elephant.svg
+${CONVERT} --export-png=${DIR}animal_kin.png dog.svg
+${CONVERT} --export-png=${DIR}animal_gin.png cat.svg
+${CONVERT} --export-png=${DIR}animal_nari_gin.png red_cat.svg
+${CONVERT} --export-png=${DIR}animal_kei.png bunny.svg
+${CONVERT} --export-png=${DIR}animal_nari_kei.png red_bunny.svg
+${CONVERT} --export-png=${DIR}animal_kyo.png pig.svg
+${CONVERT} --export-png=${DIR}animal_nari_kyo.png red_pig.svg
